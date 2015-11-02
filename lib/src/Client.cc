2756ea048beceb57d43671c1415a84eda177753b
Ali Volkan ATLI <avatli@yahoo.com>
2015-11-02 14:57:12 +0200

    Update Client.cc
    When we have an error on the connection between pairs, we need to delete the socket conn before try to connect again.

--- lib/src/Client.cc
+++ lib/src/Client.cc
@@ -84,11 +84,7 @@ ResponseClass *Client::apiCall(RequestClass *request)
   if (!this->prepareConnection()) { E("Client::apiCall():unable to create connection"); return NULL; }
 
   int status = this->sendRequest(request);
-  if (status == Connection::WRITE_ERROR)
-  {
-    E("Client::apiCall():sendRequest() error:" << strerror(errno) << "\n");
-    return NULL;
-  }
+  if (status == Connection::WRITE_ERROR) { E("Client::apiCall():sendRequest() error:" << strerror(errno) << "\n"); return NULL; }
 
   D(cout.flush() << "Client::apiCall:" << typeid(RequestClass).name() << " sent:\n" << *request;)
 
@@ -112,7 +108,11 @@ int Client::sendRequest(Request *request)
 
   unsigned char *buffer = request->toWireFormat();
   int numBytesSent = this->connection->write(request->size(), buffer);
-  if (numBytesSent == Connection::WRITE_ERROR) { E("Client::sendRequest():write error:" << strerror(errno) << "\n"); return numBytesSent; }
+  if (numBytesSent == Connection::WRITE_ERROR) {
+    E("Client::sendRequest():write error:" << strerror(errno) << "\n");
+    delete this->connection; this->connection = NULL;
+    return numBytesSent;
+  }
   D(cout.flush() << "Client::sendRequest():request sent:numBytes:" << numBytesSent << "\n";)
   return numBytesSent;
 }
@@ -126,13 +126,21 @@ ResponseClass *Client::receiveResponse()
 
   int netValueSize = -1;
   int numBytesReceived = this->connection->read(sizeof(int), (unsigned char *)(&netValueSize));
-  if (numBytesReceived == Connection::READ_ERROR) { E("Client::receiveResponse():read error on size:" << strerror(errno) << "\n"); return NULL; }
+  if (numBytesReceived == Connection::READ_ERROR || numBytesReceived == Connection::END_OF_CONNECTION_ERROR) {
+    E("Client::receiveResponse():read error on size:" << strerror(errno) << "\n");
+    delete this->connection; this->connection = NULL;
+    return NULL;
+  }
   int hostValueSize = ntohl(netValueSize);
   D(cout.flush() << "Client::receiveResponse():incoming response:size:" << hostValueSize << "\n";)
   unsigned char *buffer = new unsigned char[hostValueSize+sizeof(int)]; // add space for int32 size
   memcpy(buffer, &netValueSize, sizeof(int));
   numBytesReceived = this->connection->read(hostValueSize, buffer + sizeof(int));
-  if (numBytesReceived == Connection::READ_ERROR) { E("Client::receiveResponse():read error on body:" << strerror(errno) << "\n"); return NULL; }
+  if (numBytesReceived == Connection::READ_ERROR || numBytesReceived == Connection::END_OF_CONNECTION_ERROR) {
+    E("Client::receiveResponse():read error on body:" << strerror(errno) << "\n");
+    delete this->connection; this->connection = NULL;
+    return NULL;
+  }
   return new ResponseClass(buffer, true); // true specfies delete buffer on ~Response()
 }
 
